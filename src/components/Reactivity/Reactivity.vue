65dd1c83179a57cdfac30880c263907e303af5a8
Patrik Švikruha <patrik.svikruha@gmail.com>
2022-03-28 10:05:50 +0200

    refactor Reactivity

--- src/components/Reactivity/Reactivity.vue
+++ src/components/Reactivity/Reactivity.vue
@@ -6,13 +6,8 @@
 </template>
 
 <script setup lang="ts">
-import Watch from "./Watch/Watch.vue";
-import Props from "./Props/Props.vue";
-import Computed from "./Computed.vue";
 import Ref from "./Ref.vue";
 import Reactive from "./Reactive.vue";
-
+import Computed from "./Computed.vue";
+import Watch from "./Watch/Watch.vue";
 </script>
-
-<style scoped>
-</style>
