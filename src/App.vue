9995f79a9b43d1a3399acac39cef52e7b24c44bf
Patrik Švikruha <patrik.svikruha@gmail.com>
2022-03-28 09:56:05 +0200

    improve examples "setup"

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
     <div class="center">
-        <!-- <Reactivity /> -->
-        <Conditions />
+        <input type="checkbox" v-model="reactivity" id="reactivity" />
+        <label for="reactivity">Reactivity</label>
+        <input type="checkbox" v-model="conditions" />
+        <label for="conditions">Conditions</label>
+        <input type="checkbox" v-model="props" />
+        <label for="props">Props</label>
+        <input type="checkbox" v-model="events" />
+        <label for="events">Events</label>
+
+        <Reactivity v-if="reactivity" />
+        <Conditions v-if="conditions" />
+        <Props v-if="props" />
+        <Events v-if="events" />
         <!-- <MyStyleComponent/> -->
         <!-- <Conditions/> -->
-        <!-- <Reactivity /> -->
+
+        <!-- <TreeView/> -->
     </div>
 </template>
 
 <script setup lang="ts">
-import MyStyleComponent from './components/Style/MyStyleComponent.vue';
-import Conditions from './components/Conditions/Conditions.vue';
 import Reactivity from './components/Reactivity/Reactivity.vue';
+import Conditions from './components/Conditions/Conditions.vue';
+import Props from './components/Props/Props.vue';
+
+import MyStyleComponent from './components/Style/MyStyleComponent.vue';
+import TreeView from './exercises/TreeView/TreeView.vue';
+import { ref } from 'vue';
+import Events from './components/Events/Events.vue';
+
+const reactivity = ref(false)
+const conditions = ref(false)
+const props = ref(false)
+const events = ref(false)
 
 </script>
 
