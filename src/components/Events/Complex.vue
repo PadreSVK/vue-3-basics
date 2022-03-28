c3a50c83ef3d832683a6bbfba97c1140e17ccd97
Patrik Švikruha <patrik.svikruha@gmail.com>
2022-03-28 12:36:15 +0200

    add typed events

---
+++ src/components/Events/Complex.vue
@@ -0,0 +1,41 @@
+<template>
+    <h1>Complex events</h1>
+    <button @click="emitWithoutPayload">Emit without payload</button>
+    <button @click="emitWithPayload">Emit with payload</button>
+
+    <!-- $event => native Event in handler -->
+    <button @click="logNativeEvent">log native Event (implicit argument)</button>
+    <button @click="logNativeEvent($event)">log native Event (explicit argument)</button>
+</template>
+
+<script setup lang="ts">
+
+// const emit = defineEmits(["btn-clicked", "btn-clicked-payload"])
+// interface Payload {
+//     test: string
+//     age: number
+// }
+
+const emit = defineEmits<{
+    (e: "btn-clicked"): void,
+    (e: "btn-clicked-payload", value: { test: string, age: number }): void,
+    // (e: "btn-clicked-payload", value: Payload): void,
+}>()
+
+
+
+function emitWithoutPayload() {
+    console.log("emiting event....")
+    emit('btn-clicked')
+}
+function emitWithPayload() {
+    console.log("emiting event....")
+    emit('btn-clicked-payload', { test: "Fero", age: 999 })
+}
+
+function logNativeEvent(event: Event) {
+    console.log(event)
+}
+
+</script>
+
--- src/components/Events/Events.vue
+++ src/components/Events/Events.vue
@@ -2,20 +2,27 @@
     <h1>Events</h1>
     <SimpleEvents />
     <Emit @btn-clicked="btnClicked" @btn-clicked-payload="btnClickedPayload" />
+    <Complex @btn-clicked="btnClicked" @btn-clicked-payload="btnClickedPayload" />
 </template>
 
 <script setup lang="ts">
 
 import SimpleEvents from "./SimpleEvents.vue";
 import Emit from "./Emit.vue";
+import Complex from "./Complex.vue";
 
 function btnClicked() {
     console.log("btnClicked => without payload");
 }
 
-function btnClickedPayload(payload: any) {
+// without deconstruct
+// function btnClickedPayload(value : { test: string, age: number} ) {
+
+//with deconstruct
+function btnClickedPayload({ age, test }: { test: string, age: number }) {
     console.log("btnClickedPayload => with payload");
-    console.log({ payload })
+    console.log({ test })
+    console.log({ age })
 }
 
 </script>
