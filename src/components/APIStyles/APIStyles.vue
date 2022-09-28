d6e94f916c07845e0dd2be77d548fe627f850f10
Patrik Švikruha <patrik.svikruha@gmail.com>
2022-09-28 08:37:13 +0200

    Add api styles

---
+++ src/components/APIStyles/APIStyles.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+    <CompositionAPI></CompositionAPI>
+    <CompositionSetupAPI></CompositionSetupAPI>
+    <OptionsAPI></OptionsAPI>
+</template>
+
+<script setup lang="ts">
+import CompositionAPI from "./CompositionAPI.vue";
+import CompositionSetupAPI from "./CompositionSetupAPI.vue";
+import OptionsAPI from "./OptionsAPI.vue";
+
+</script>
---
+++ src/components/APIStyles/CompositionSetupAPI.vue
@@ -0,0 +1,28 @@
+<script>
+import { ref, onMounted } from 'vue'
+
+export default {
+    setup() {
+        const count = ref(0)
+
+        function increment() {
+            count.value++
+        }
+
+        // lifecycle hooks
+        onMounted(() => {
+            console.log(`The initial count is ${count.value}.`)
+        })
+        // don't forget to expose the function as well.
+        return {
+            count,
+            increment,
+            onMounted
+        }
+    }
+}
+</script>
+
+<template>
+    <button @click="increment">Count is: {{ count }}</button>
+</template>
